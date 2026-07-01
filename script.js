fbcd0884fd572af54d69d86adcb2dd07df0c1c61
https://Abhishek.github.io <as9839786@gmail.com>
2026-07-02 02:06:38 +0530

    Remove photo slideshow code
    Removed photo slideshow functionality from script.js.

--- script.js
+++ script.js
@@ -3,24 +3,4 @@ document.getElementById("startBtn").onclick = function(){
 window.location.href="pages/mission1.html";
 
 }
-const photos=[
-"../assets/photos/photo1.jpg",
-"../assets/photos/photo2.jpg",
-"../assets/photos/photo3.jpg",
-"../assets/photos/photo4.jpg",
-"../assets/photos/photo5.jpg"
-];
 
-let photoIndex=0;
-
-setInterval(function(){
-
-photoIndex++;
-
-if(photoIndex>=photos.length){
-photoIndex=0;
-}
-
-document.getElementById("photo").src=photos[photoIndex];
-
-},3000);
